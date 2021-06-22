f3b18911fe2418125a11cb451a59aeeac82409b7
Joseph <josephsasson1@gmail.com>
2021-06-22 17:31:00 -0400

    Add player name and team

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+# Objective
+This project is going to showcase how to add information about players from an API onto the browser.
 
+# Main Deliverables
+- Add an event listener that pulls up rest of player information when name is clicked.
+- Use search bar to search by player name functionality.
+- Be able to sort by team name.
--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 document.addEventListener('DOMContentLoaded',()=>{
-  console.log('hi')
+  getInfo()
 })
+
+const getInfo = () =>{
+  fetch('https://www.balldontlie.io/api/v1/players?per_page=100')
+  .then(res=>res.json())
+  .then(data=>data.data.forEach(createTable))
+}
+
+const createTable = info =>{
+  const body = document.querySelector('body')
+  const div = document.createElement('div')
+  div.id = 'table-div'
+  const table = document.createElement('table')
+  const tableHeader = document.createElement('thead')
+  const row = document.createElement('tr')
+  const name = document.createElement('td')
+  name.textContent = info.first_name + ' ' + info.last_name
+  const team = document.createElement('td')
+  team.textContent = info.team.full_name
+  row.append(name,team)
+  tableHeader.append(row)
+  table.append(tableHeader)
+  div.append(table)
+  body.append(div)
+}
+
