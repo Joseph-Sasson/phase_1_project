70fd2052ad973dcba06226c0e2de3ab3f1f04d11
Joseph <josephsasson1@gmail.com>
2021-06-24 10:23:38 -0400

    update index.html and removed duplicate teams in index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded',()=>{
   getInfo()
-  const body = document.querySelector('body')
+  const main = document.querySelector('#main')
   const header = document.createElement('h1')
   header.textContent = 'My Player Profile'
   const west = document.createElement('h1')
@@ -9,25 +9,42 @@ document.addEventListener('DOMContentLoaded',()=>{
   const east = document.createElement('h1')
   east.id = 'EAST'
   east.textContent = 'East'
-  body.append(header,west,east)
+  main.append(header,west,east)
 })
 
+let allPlayers = []
+let teams = []
+
 const getInfo = () =>{
-  // for(let i = 0; i < 36; i++)
-  fetch(`https://www.balldontlie.io/api/v1/players?per_page=25`)
+  for(let i = 0; i < 36; i++)
+  fetch(`https://www.balldontlie.io/api/v1/players?per_page=100&page=${i}`)
   .then(res=>res.json())
-  .then(data=>data.data.forEach(createTable))
+  .then(data=>{
+    allPlayers = [...allPlayers, ...data.data]})
 }
+
+const renderPlayers = () =>{
+  const main = document.querySelector('#main')
+  const loading = document.querySelector('#loading')
+  loading.style.display = 'none'
+  main.style.display = 'block'
+  let allTeams = allPlayers.map(player=>player.team)
+  allTeams.forEach(team=>{
+    if (!teams.filter(t => t.id === team.id).length > 0){teams.push(team)}
+  })
+  teams.forEach(createTable)
+}
+
+setTimeout(renderPlayers,1500)
 
 const createTable = info =>{
   const west = document.querySelector('#WEST')
   const east = document.querySelector('#EAST')
   const westTeams = document.createElement('div')
   const eastTeams = document.createElement('div')
-  if (info.team.conference === 'West'){
-    westTeams.textContent = info.team.full_name}else{
-      eastTeams.textContent = info.team.full_name}
+  if (info.conference === 'West'){
+    westTeams.textContent = info.full_name}
+    else {eastTeams.textContent = info.full_name}
   west.append(westTeams)
   east.append(eastTeams)
   }
-  
